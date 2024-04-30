21260ffa0fb885e848b823fc35a886cf24723c00
hyoredm <hyoredm@gmail.com>
2024-04-30 18:07:29 +0900

    boj11286 solve!

--- boj11286/Main.java
+++ boj11286/Main.java
@@ -3,6 +3,9 @@ package boj11286;
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+
+        int N = scan.nextInt();
+
     }
 }
