7d23192bc6333fa072223516dfa8274dfc4cf601
hyoredm <hyoredm@gmail.com>
2024-05-05 22:27:41 +0900

    boj5014 solving!

--- boj5014/Main.java
+++ boj5014/Main.java
@@ -2,6 +2,8 @@ package boj5014;
 
 import java.util.*;
 public class Main {
+    static int[]graph;
+    static boolean[]visit;
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
@@ -12,21 +14,43 @@ public class Main {
         int u = scan.nextInt();
         int d = scan.nextInt();
 
-        int answer = 0;
+        graph = new int[f+1];
+        visit = new boolean[f+1];
 
-        while(s < g) {
-            s += u;
-            answer++;
-        }
+        int[]dy = {u, -d};
 
-        while(s != g) {
-            s -= d;
-            answer++;
-            if(s < g && d == 0) {
-                System.out.println("use the stairs");
-                return;
+        Boolean flag = false;
+
+        Queue<Integer> q = new LinkedList<>();
+
+        q.offer(s);
+        visit[s] = true;
+
+        while(!q.isEmpty()) {
+            for(int i = 0; i < q.size(); i++) {
+                int num = q.poll();
+
+                if(num == g) {
+                    flag = true;
+                    break;
+                }
+
+                for(int j = 0; j < 2; j++) {
+                    int ny = num + dy[j];
+
+                    if(ny > 0 && ny <= f) {
+                        if(!visit[ny]) {
+                            visit[ny] = true;
+                            graph[ny] = graph[num] + 1;
+                            q.offer(ny);
+                        }
+                    }
+                }
             }
+            if(flag) break;
         }
-        System.out.println(answer);
+        if(!visit[g]) {
+            System.out.println("use the stairs");
+        } else System.out.println(graph[g]);
     }
 }
