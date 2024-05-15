784ba9573082648128c5e43c1d7fe119b9ad2666
hyoredm <hyoredm@gmail.com>
2024-05-15 23:49:57 +0900

    try boj1707

--- boj1707/Main2.java
+++ boj1707/Main2.java
@@ -1,12 +1,32 @@
 package boj1707;
 
 import java.util.*;
-public class Main {
+public class Main2 {
 
     static List<Boolean> visit;
 
-//    static boolean[] visit;
-    static int[][] graph;
+    //    static boolean[] visit;
+//    static int[][] graph;
+
+    static class Pair {
+        int x = 0;
+        int y = 0;
+        int value = 0;
+
+         Pair(int x, int y, int value) {
+            this.x = x;
+            this.y = y;
+            this.value = value;
+
+        }
+
+        Pair(int x) {
+             this.x = x;
+        }
+    }
+
+    static List<Pair> graph;
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
@@ -17,17 +37,20 @@ public class Main {
             int E = scan.nextInt();
 
 //            visit = new boolean[V+1];
-            graph = new int[V+1][V+1];
+//            graph = new int[V+1][V+1];
             visit = new ArrayList<>();
+            graph = new ArrayList<>();
             for(int j = 0; j <= V; j++) {
                 visit.add(false);
+                graph.add(new Pair(j));
             }
 
             for(int j = 0; j < E; j++) {
                 int u = scan.nextInt();
                 int v = scan.nextInt();
 
-                graph[u][v] = 1;
+                graph.set(u, new Pair(u, v, 1));
+                System.out.println(u + "의 value : " + graph.get(u).value);
             }
 
             int cnt = 0;
@@ -55,9 +78,10 @@ public class Main {
     static void dfs(int V, int start) {
         visit.set(start, true);
         for(int i = 1; i <= V; i++) {
-            if(!visit.get(i) && graph[start][i] == 1) {
+            if(!visit.get(i) && graph.get(i).value == 1) {
                 dfs(V, i);
             }
         }
     }
 }
+
