7072a20c7aa8e2edbae8de5dd74a4e0fb37de1f0
hyoredm <hyoredm@gmail.com>
2024-05-19 23:15:32 +0900

    try pro42583!

---
+++ pro42583/Solution.java
@@ -0,0 +1,10 @@
+package pro42583;
+
+import java.util.*;
+
+class Solution {
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        int answer = 0;
+        return answer;
+    }
+}
