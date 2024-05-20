fb835dcf711d46f8722684212699f8f1b60c0c98
hyoredm <hyoredm@gmail.com>
2024-05-20 23:47:16 +0900

    solve pro42584!

--- pro42583/Solution.java
+++ pro42583/Solution.java
@@ -5,6 +5,18 @@ import java.util.*;
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
         int answer = 0;
+
+        Queue<Integer> passed = new LinkedList<>();
+        Queue<Integer> passing = new LinkedList<>();
+        Queue<Integer> waiting = new LinkedList<>();
+
+        for(int i = 0; i < truck_weights.length; i++) {
+            waiting.add(truck_weights[i]);
+        }
+
+        answer++; // answer 1
+
+
         return answer;
     }
 }
---
+++ pro42584/Solution.java
@@ -0,0 +1,28 @@
+package pro42584;
+
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+
+        int cnt = 0;
+
+        for(int i = 0; i < prices.length; i++) {
+            cnt = 0;
+            if(i == prices.length-1) {
+                answer[i] = 0;
+            } else {
+                for(int j = i + 1; j < prices.length; j++) {
+                    cnt++;
+                    if(prices[i] > prices[j]) {
+                        break;
+                    }
+                }
+                answer[i] = cnt;
+            }
+        }
+
+        return answer;
+    }
+}
