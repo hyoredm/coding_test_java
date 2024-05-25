9de3dd4f8fa2e9418880f551ffcf55bd257c05e9
hyoredm <hyoredm@gmail.com>
2024-05-25 23:00:58 +0900

    solve pro42626!

--- pro42626/Solution.java
+++ pro42626/Solution.java
@@ -9,25 +9,31 @@ class Solution {
         PriorityQueue<Integer> pq = new PriorityQueue<>();
 
         for(int i = 0; i < scoville.length; i++) {
+            // if(scoville[i] < K)
             pq.add(scoville[i]);
         }
+
+        int cnt = 0;
 
         while(pq.size() > 1) {
             int f1 = 0;
             int f2 = 0;
             int nf = 0;
-            int cnt = 0;
 
             if(pq.peek() < K) {
                 f1 = pq.poll();
                 f2 = pq.poll();
-                nf = f1 + f2;
+                nf = f1 + (f2*2);
                 pq.add(nf);
                 cnt++;
                 answer = cnt;
-            } else if(pq.peek() > K) {
+            } else if(pq.peek() >= K) {
                 break;
             }
+        }
+
+        if(pq.peek() < K) {
+            answer = -1;
         }
 
 
