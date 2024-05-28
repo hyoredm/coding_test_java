4f18df71b6c9871f1a6d82027073066cd6d3b5a8
hyoredm <hyoredm@gmail.com>
2024-05-28 23:54:27 +0900

    try pro42628!

--- pro42627/Solution.java
+++ pro42627/Solution.java
@@ -3,6 +3,15 @@ package pro42627;
 import java.util.*;
 
 class Solution {
+    static class Pair{
+        int start;
+        int duration;
+
+        public Pair(int start, int duration) {
+            this.start = start;
+            this.duration = duration;
+        }
+    }
     public int solution(int[][] jobs) {
         int answer = 0;
         return answer;
