3b2fa808be6e7cf0cb54007839eb174bdf885d0c
hyoredm <hyoredm@gmail.com>
2025-06-01 07:32:36 +0900

    solve leetcode21!

--- leetcode21/Solution.java
+++ leetcode21/Solution.java
@@ -13,22 +13,29 @@ package leetcode21;
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
 
-        ListNode result = null;
+        ListNode result = new ListNode();
+        ListNode curr = result;
 
-        if(list1 == null) {
-            return list2;
-        }
-        else if(list2 == null) {
-            return list1;
-        }
-
-        if(list1 != null && list2 != null) {
-            if(list1.val >= list2.val) {
-
+        while(list1 != null && list2 != null) {
+            if(list1.val <= list2.val) {
+                curr.next = list1;
+                list1 = list1.next;
             }
+            else if (list1.val > list2.val) {
+                curr.next = list2;
+                list2 = list2.next;
+            }
+            curr = curr.next;
         }
 
-        return result;
+        if(list1 == null && list2 != null) {
+            curr.next = list2;
+        }
+        else if(list1 != null && list2 == null) {
+            curr.next = list1;
+        }
+
+        return result.next;
     }
 
     public class ListNode {
