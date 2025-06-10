fb786e6716e3f2b6a9760f97a892c60c6acf7278
hyoredm <hyoredm@gmail.com>
2025-06-10 23:31:14 +0900

    solve leetcode100!

--- leetcode100/Solution.java
+++ leetcode100/Solution.java
@@ -17,7 +17,6 @@ package leetcode100;
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-
         if(p == null && q == null) return true;
         if(p == null || q == null) return false;
         if(p.val != q.val) return false;
