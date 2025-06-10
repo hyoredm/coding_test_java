a62d4a7db314476ec51d5cef1ba929bc412a2e71
hyoredm <hyoredm@gmail.com>
2025-06-10 23:28:58 +0900

    solve leetcode100!

--- leetcode100/Solution.java
+++ leetcode100/Solution.java
@@ -18,50 +18,11 @@ package leetcode100;
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
 
-        boolean flag = true;
+        if(p == null && q == null) return true;
+        if(p == null || q == null) return false;
+        if(p.val != q.val) return false;
 
-        if(p != null && q != null) {
-            if (p.val != q.val) flag = false;
-
-            TreeNode pLft = p;
-            TreeNode qLft = q;
-            TreeNode pRt = p;
-            TreeNode qRt = q;
-
-            while (pLft.left != null || qLft.left != null) {
-                if (pLft.left != null && qLft.left != null) {
-                    if (pLft.left.val != qLft.left.val) return flag = false;
-                } else if (pLft.left == null && qLft.left != null) {
-                    return flag = false;
-                } else if (pLft.left != null && qLft.left == null) {
-                    return flag = false;
-                } else if (pLft.left == null && qLft.left == null) {
-                    return flag = false;
-                }
-                pLft = pLft.left;
-                qLft = qLft.left;
-            }
-
-            while (pRt.right != null || qRt.right != null) {
-                if (pRt.right != null && qRt.right != null) {
-                    if (pRt.right.val != qRt.right.val) flag = false;
-                } else if (pRt.right == null && qRt.right != null) {
-                    flag = false;
-                } else if (pRt.right != null && qRt.right == null) {
-                    flag = false;
-                }
-                pRt = pRt.left;
-                qRt = qRt.left;
-            }
-        }
-        else if(p == null && q!= null) {
-            flag = false;
-        }
-        else if(p != null && q == null) {
-            flag = false;
-        }
-
-        return flag;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 
     public class TreeNode {
